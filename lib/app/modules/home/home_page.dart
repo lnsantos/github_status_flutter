69b699113ca13e0887004b96026954d6a86132fb
Lucas <lucas.nepomuceno1999@gmail.com>
2020-01-12 19:55:55 -0300

    basic designer home_page

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -6,22 +6,53 @@ import '../../app_module.dart';
 
 class HomePage extends StatelessWidget {
   final String title;
-  HomePage({Key key, this.title = "Home"}) : super(key: key);
+  HomePage({Key key, this.title = "GITHUB STATUS"}) : super(key: key);
 
   final AppController mainModule = AppModule.to.get();
+
+  TextStyle _titleStyle(){
+      return TextStyle(color: Colors.white,fontSize: 30.0);
+  }
+
+  TextStyle _simpleTextStyle(){
+      return TextStyle(color: Colors.white,fontSize: 20.0);
+  }
+
+   TextStyle _textFieldStyle(){
+      return TextStyle(color: Colors.black,fontSize: 20.0,backgroundColor: Colors.white);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child:Text(
+                title,style: _titleStyle()
+                )
+                ),
+            padding: EdgeInsets.all(20.0)),
+          TextFormField(
+            autofocus: false,
+            keyboardType: TextInputType.text,
+            style: _simpleTextStyle(),
+            decoration: InputDecoration(
+              labelText: "Name user of Github.com",
+              labelStyle: _textFieldStyle()
+            ),
+          )
+        ],
       ),
-      body: RaisedButton(
-        onPressed: mainModule.increment,
-        child: Observer(
-          builder: (_)=> Text("${mainModule.value}")
-        )     
       ),
+      )
     );
   }
 }
